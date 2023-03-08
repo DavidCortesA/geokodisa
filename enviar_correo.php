a1dbef4c656989c085f03e5bfa53ebd14f4fd9a4
David Cortez <david.cortes.ayala@hotmail.com>
2023-03-07 19:23:08 -0600

    php5, working

--- enviar_correo.php
+++ enviar_correo.php
@@ -7,15 +7,15 @@
       $message = $_POST['mensaje'];
 
       // Configurar correo electrónico
-      $to = "davido_jonas@hotmail.com";
+      $to = "info@geokodisa.com";
       $subject = "Mensaje del formulario de contacto";
       $body = "Nombre: $name\nCorreo electrónico: $email\nTeléfono: $phone\nMensaje:\n$message";
 
       // Enviar correo electrónico
       if (mail($to, $subject, $body)) {
           // Mensaje de confirmación en javascript
+          header("Location: index.html#contacto");
           echo "<script>alert('Su mensaje ha sido enviado');</script>";
-          header("Location: index.html");
           exit;
       } else {
           // Mensaje de error en javascript
